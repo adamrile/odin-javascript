9e4f75c4bd664569293cd3d54193fd86a19825e4
Adam <adamabbas42i@gmail.com>
2023-02-12 19:27:10 +0000

    Corrected the old js file and included playRound() function correctly

--- JScode.js
+++ JScode.js
@@ -1,71 +1,50 @@
+let playerSelection;
+let computerSelection;
+
+const array = ["Rock", "Paper", "Scissors"];
 function getComputerChoice() {
-  let computerChoice = Math.floor(Math.random() * 3) + 1;
-  if (computerChoice === 1) {
-    return "Rock";
-  } else if (computerChoice === 2) {
-    return "Paper";
-  } else {
-    return "Scissors";
+  return array[~~(Math.random() * array.length)];
+}
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = getPlayerChoice();
+  computerSelection = getComputerChoice().toLowerCase();
+  if (playerSelection === computerSelection) {
+    return "draw";
   }
-}
 
-function getPlayerChoice() {
-  let playerChoice = prompt("'Rock', 'Paper' or 'Scissors'?");
-  return (
-    playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase()
-  );
-}
-
-function rps(computer, player) {
-  for (let i = 1; i <= 5; i++) {
-    let result;
-    let playerChoice = getPlayerChoice();
-    let computerChoice = getComputerChoice();
-
-    if (playerChoice === "Rock" && computerChoice === "Rock") {
-      result = "It's a draw!";
-    } else if (playerChoice === "Rock" && computerChoice === "Paper") {
-      result = "You Lose";
-    } else if (playerChoice === "Rock" && computerChoice === "Scissors") {
-      result = "Congrats you won!";
-    } else if (playerChoice === "Paper" && computerChoice === "Rock") {
-      result = "You win";
-    } else if (playerChoice === "Paper" && computerChoice === "Paper") {
-      result = "Draw";
-    } else if (playerChoice === "Paper" && computerChoice === "Scissors") {
-      result = "You lose";
-    } else if (playerChoice === "Scissors" && computerChoice === "Rock") {
-      result = "You lose";
-    } else if (playerChoice === "Scissors" && computerChoice === "Paper") {
-      result = "You win";
-    } else if (playerChoice === "Scissors" && computerChoice === "Scissors") {
-      result = "Draw";
+  if (playerSelection === "rock") {
+    if (computerSelection === "scissors") {
+      return "You win! Rock beats scissors.";
     } else {
-      result = "Invalid input";
+      return "You lose! Paper beats Rock";
     }
-    console.log(result);
   }
-  console.log(" ");
-  console.log("Game over! ");
+
+  if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
+      return "You win! paper beats rock.";
+    } else {
+      return "You lose! scissors beats paper";
+    }
+  }
+
+  if (playerSelection === "scissors") {
+    if (computerSelection === "paper") {
+      return "You win! scissors beats paper.";
+    } else {
+      return "You lose! rock beats scissors";
+    }
+  } else {
+    return "Invalid input";
+  }
 }
 
 function game() {
-  rps();
+  return playRound(playerSelection, computerSelection);
 }
 
-/*
-The function is run 
-There are 3 possible options it can take (1) rock (2) Paper (3) Scissors
-the computer needs to randomly generate a number from 1 to 3 
-and each number needs to === one of the options 
-if (computerchoice === 1){ return rock }
-then the code needs to return computerchoice
-
-Round of RPS
-
-the function runs takes two parameters 
-it asks the player for their move 
-it takes the computers move
-if the human's choice is rock an
-
-*/
+function getPlayerChoice() {
+  let playerSelection = prompt("'Rock', 'Paper' or 'Scissors' ?").toLowerCase();
+  return playerSelection;
+}
