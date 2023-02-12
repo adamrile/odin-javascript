199e362dc732e2ac5bdffb46867839fe554b1f4d
Adam <adamabbas42i@gmail.com>
2023-02-12 20:05:37 +0000

    added game() function to play 5 rounds of rock papers scissors and count points of each player

--- JScode.js
+++ JScode.js
@@ -1,9 +1,12 @@
 let playerSelection;
 let computerSelection;
+let computerScore = 0;
+let playerScore = 0;
 
 const array = ["Rock", "Paper", "Scissors"];
 function getComputerChoice() {
   return array[~~(Math.random() * array.length)];
+  console.log();
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -41,7 +44,36 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-  return playRound(playerSelection, computerSelection);
+  for (let i = 1; i <= 5; i++) {
+    let result = playRound();
+    if (
+      result === "You win! Rock beats scissors." ||
+      result === "You win! paper beats rock." ||
+      result === "You win! scissors beats paper."
+    ) {
+      console.log("You beat the machine!");
+      playerScore += 1;
+    } else if (
+      result === "You lose! Paper beats Rock" ||
+      result === "You lose! scissors beats paper" ||
+      result === "You lose! rock beats scissors"
+    ) {
+      console.log("Computer won that round...Good luck on the next try!");
+      computerScore += 1;
+    } else if (result === "Invalid input") {
+      console.log("Invalid input. Please try again.");
+      i--;
+    } else if (result === "draw") {
+      console.log("its a draw!");
+    } else {
+      return "An error has occured. Please try again.";
+    }
+  }
+  if (computerScore > playerScore) {
+    return `Computer wins by ${computerScore - playerScore} points`;
+  } else if (computerScore < playerScore) {
+    return `Computer wins by ${playerScore - computerScore} points`;
+  }
 }
 
 function getPlayerChoice() {
